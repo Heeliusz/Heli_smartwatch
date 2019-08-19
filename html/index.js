cb309030594723a3221ac3ff242182f9b5469bb4
Heeliusz <47533297+Heeliusz@users.noreply.github.com>
2019-08-19 20:54:04 +0200

    Add files via upload

---
+++ html/index.js
@@ -0,0 +1,56 @@
+
+$(function () {
+    function display(bool) {
+        if (bool) {
+            $("#container").show();
+        } else {
+            $("#container").hide();
+        }
+    }
+
+    display(false)
+
+    window.addEventListener('message', function(event) {
+        var item = event.data;
+        if (item.type === "ui") {
+            if (item.status == true) {
+                display(true)
+            } else {
+                display(false)
+            }
+        }
+    })
+    // if the person uses the escape key, it will exit the resource
+    document.onkeyup = function (data) {
+        if (data.which == 27) {
+            $.post('http://Heeliusz_smartwatcg/exit', JSON.stringify({}));
+            return
+        }
+    };
+    
+
+
+    $("#mandatbtn").click(function(){
+		$('#menu').hide();
+		$('#pasek1').fadeIn(1000);
+	});	
+
+})    
+
+
+
+    function startTime() {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById('menu').innerHTML =
+        h + ":" + m + ":" + s;
+        var t = setTimeout(startTime, 500);
+      }
+      function checkTime(i) {
+        if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+        return i;
+      }
